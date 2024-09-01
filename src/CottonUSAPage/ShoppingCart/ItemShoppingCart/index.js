ab20b9b87f227bc6af880746ebb04d9160710d70
ducanh1773 <anhductrannguyen170703@gmail.com>
2024-09-01 11:42:19 +0700

    main

---
+++ src/CottonUSAPage/ShoppingCart/ItemShoppingCart/index.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import "./index.css";
+import { Link } from "react-router-dom";
+import { InformationPrd } from "../../cottonUSAReuse";
+function ItemShoppingCart(props) {
+  const [value, setValue] = useState(1); // Default value set to 1
+  const [sumPrice, setSumPrice] = useState(119000); // Initial sumPrice
+  const [showPrd , setShowPrd ]  = useState(true);
+  const handleInputChange = (event, price) => {
+    const newValue = event.target.value;
+    setValue(newValue);
+
+    // Recalculate the sum price when value changes
+    setSumPrice(newValue * price);
+  };
+
+  const  handleClickDeletePrd = () =>{
+    setShowPrd(false);
+  }
+
+  const Information = props.InformationPrd.map((t) => (
+    <div className="allItemObjectShoppingCart" key={t.id}>
+      <div className="oneItermPrdOfItemObjectShoppingCart">
+        <div className="imgOfShoppingCart">
+          <img src={t.imgUrl1} alt={t.name} />
+        </div>
+        <div className="mainRightOfOneItermPrdOfItemObjectShoppingCart">
+          <div className="namePrdOfItemObjectShoppingCart">
+            <Link>
+              <h4>{t.name}</h4>
+            </Link>
+          </div>
+          <div className="pricePrdOfItemObjectShoppingCart">
+            <p>{t.price}đ</p>
+          </div>
+          <div className="sizePrdOfItemObjectShoppingCart">
+            <p>{t.size}</p>
+          </div>
+        </div>
+      </div>
+      <div className="numberPrdOfItemObjectShoppingCart">
+        <form>
+          <input
+            type="number"
+            value={value}
+            onChange={(event) => handleInputChange(event, t.price)}
+            min="1" // Ensure the quantity is at least 1
+          />
+          <p onClick={handleClickDeletePrd}>Gỡ bỏ</p>
+        </form>
+      </div>
+      <div>
+        <p>{sumPrice}đ</p> {/* Display updated sumPrice */}
+      </div>
+    </div>
+  ));
+
+
+  return <div style={{ display: "flex" }}>
+    {showPrd&&<div>
+    {Information}
+    </div>}
+    </div>;
+}
+
+export default ItemShoppingCart;
--- src/CottonUSAPage/ShoppingCart/index.js
+++ src/CottonUSAPage/ShoppingCart/index.js
@@ -1,23 +1,45 @@
 import React from "react";
 import "./index.css"
 import HeaderCottonUSA from "../HeaderCottonUSA";
+import ItemShoppingCart from "./ItemShoppingCart/index";
+import { useState } from "react";
 function ShoppingCart(){
+    const [info6, setInfo6] = useState([
+        {
+          imgUrl1: "https://cottonusa.co/cdn/shop/files/1_6.jpg?v=1696431420&width=800",
+          name: "MLB Los Angeles Dodgers Logo Red T-Shirt",
+          price: "119.000",
+          size:"XL"
+        
+        },
+
+      
+      ]);
     return(
         <div>
             <HeaderCottonUSA></HeaderCottonUSA>
             <div className="subjectShoppingCart">
                 <h1>Giỏ hàng</h1>
             </div>
-            <div className="">
-             <div className="">
-                <h4>Sản phẩm</h4>
-                <h4>Số lượng</h4>
+            <div className="allObjectOfShoppingCart">
+             <div className="mainLeftShoppingCart">
+                <div className="objectOfShoppingCart">
+                <h4 className="prdOfObjectOfShoppingCart">Sản phẩm</h4>
+                <h4 className="numberOfObjectOfShoppingCart">Số lượng</h4>
                 <h4>Tổng cộng</h4>
+                </div>
+                <div className="">
+                    <div>
+                    <ItemShoppingCart InformationPrd={info6}></ItemShoppingCart>
+                    </div>
+                   
+                </div>
              </div>
-             <div>
-                    <h3>Tổng phụ</h3>
-                    <h2>Tổng cộng</h2>
+             <div className="mainRightShoppingCart">
+                    <h3>Tổng phụ : 119.000</h3>
+                    <h2>Tổng cộng : 119.000</h2>
                     <textarea placeholder="Ghi chú đặt hàng"></textarea>
+                    
              </div>
             </div>
 
